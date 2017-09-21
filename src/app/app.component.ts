caa55ca5cfa8af9324685e4bebc059b80a7d49a1
Sean Sutherland <seansutherland@Seans-iMac.local>
2017-09-20 22:13:39 -0600

    Moved html template and style to separate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-	template: '<h2>Hello World!</h2>',
-	styles: [
-       `
-       h2 {
-       		font-family: sans-serif;
-       		font-size: 1.2em;
-       }
-       `
-	]
-
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css']
 		
 })
 export class AppComponent{
-
+	emoji = ['🎉', '😍', '😜', '👍'];
 }

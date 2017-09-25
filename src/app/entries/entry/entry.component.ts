c055d18f303549450e69d95fd6247a09a8aa8437
Sean Sutherland <seansutherland@Seans-iMac.local>
2017-09-24 22:51:18 -0600

    Soooo some structural directives here to get the comments sections going

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -10,4 +10,10 @@ export class EntryComponent{
  	title: string = 'My First Photo';
  	photo: string = 'http://placehold.it/800x500?text=Angular Basics';
  	description: string = 'A Description of my first photo';
+ 	comments: any[] = [
+ 		{name: "John", comment: "A comment"},
+ 		{name: "Jim", comment: "A comment"},
+ 		{name: "Jenn", comment: "A comment"},
+
+ 	]
 }
